76673116c2f7e3075308e03b6a3b662e1950ed85
venatus59 <seanfranklin@u.boisestate.edu>
2015-06-30 11:48:39 -0600

    initial set up

---
+++ SimpleFairyTale.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package simpleFairyTale;
+
+/**
+ * @author SAIT-admin
+ *
+ */
+public class SimpleFairyTale {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ XMLReader.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package simpleFairyTale;
+
+/**
+ * @author SAIT-admin
+ *
+ */
+public class XMLReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
