422b98b81c1d9f06ccbe0e4e1d5b907a562c1090
venatus59 <seanfranklin@u.boisestate.edu>
2015-07-06 13:51:40 -0600

    some updates

--- src/simpleFairyTale/XMLParser.java
+++ src/simpleFairyTale/XMLParser.java
@@ -4,6 +4,10 @@
 package simpleFairyTale;
 
 import javax.xml.*;
+
+import org.w3c.dom.*;
+
+import java.io.File;
 /**
  * @author ActualAdmin
  *
@@ -15,6 +19,14 @@ public class XMLParser {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try{
+			File current = new File("the Villiage.xml");
+			System.out.println("read the file");
+		}
+		
+		catch (Exception e){
+			e.printStackTrace();
+		}
 
 	}
 
